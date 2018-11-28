0b6eac45523d9ae35054b9a71579b8801818e35b
Fernando Olea <fernando.olea76@gmail.com>
2018-11-28 19:12:56 -0200

    adds job to generate report

--- spec/jobs/report_job_spec.rb
+++ spec/jobs/report_job_spec.rb
@@ -1,12 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ReportJob do
-  it 'enqueues a ReportJob with params' do
-    params = { some: 'param' }
+  context 'when everything is fine' do
+    it 'enqueues a ReportJob with params' do
+      ActiveJob::Base.queue_adapter = :test
+      params = { some: 'param' }
 
-    ActiveJob::Base.queue_adapter = :test
-    ReportJob.perform_later(params)
+      ReportJob.perform_later(params)
 
-    expect(ReportJob).to have_been_enqueued.with(params)
+      expect(ReportJob).to have_been_enqueued.with(params)
+    end
+
+    it 'calls open on CSV' do
+      job = ReportJob.new
+      path = Rails.root.join('tmp', 'reports')
+      file = "#{path}/products-report#{Time.now.to_i}.csv"
+
+      expect(CSV).to receive(:open).with(file, 'w')
+      
+      job.perform
+    end
+
+    it 'returns true' do
+      job = ReportJob.new
+
+      expect(job.perform).to be true
+    end
+  end
+
+  context 'when File operation raises some error' do
+    it 'returns false' do
+      allow(CSV).to receive(:open).and_raise('Something went wrong!')
+
+      job = ReportJob.new
+
+      expect(job.perform).to be false
+    end
   end
 end
